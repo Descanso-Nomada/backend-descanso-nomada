625aaa136a042c06c2d7e4941c60652bd3ed5e81
HaroruDa3 <harol.kock34@gmail.com>
2024-05-06 22:11:44 -0600

    VALIDACIONES DE REGISTROS COMPLETAS
    SE CREARON LAS VALIDACIONES EN EL BACKEND PARA EL REGISTRO Y PARA EL INICIO DE SESION

--- routes/apiUsuarios.js
+++ routes/apiUsuarios.js
@@ -1,10 +1,11 @@
 import Express from 'express';
 const apiUsuarios = Express();
+import { validateRegistroUsuario } from '../validators/registroValidator.js';
 import {registrarUsuario, eliminarUsuario, actualizarContrasenia, mostrarUsuarios } from '../controllers/usuariosController.js';
 import { validarCookie } from '../helpers/helpersCookie.js';
 
-//registrar un usuario nuevo
-apiUsuarios.post('/registro', registrarUsuario);
+
+apiUsuarios.post('/registro',validateRegistroUsuario, registrarUsuario);
 // apiUsuarios.get('/:id',obtenerUsuarioporId);
 apiUsuarios.get('/clientes',mostrarUsuarios);
 apiUsuarios.put('',actualizarContrasenia);
