858887eff747105423d868cd33bc814ffe41fa50
HaroruDa3 <harol.kock34@gmail.com>
2024-05-12 15:10:51 -0600

    Revert "contact"
    This reverts commit 98ed829215b459fa37c5c5a390a4f9a672681689.

--- routes/apiReservaciones.js
+++ routes/apiReservaciones.js
@@ -1,10 +1,9 @@
 import Express from 'express';
 const apiReservaciones = Express();
 import {crearReservacion, obtenerReservaciones, reservacionesUsuario, actualizarReservacion, eliminarReservacion} from '../controllers/reservacionesController.js';
-import { validateReservacion } from '../validators/reservacioneValidator.js';
 import { validarCookie } from '../helpers/helpersCookie.js';
 
-apiReservaciones.post('/reservar',validateReservacion, validarCookie, crearReservacion);
+apiReservaciones.post('/reservar',validarCookie, crearReservacion);
 apiReservaciones.get('/reservaciones-hoteles', validarCookie, obtenerReservaciones);
 apiReservaciones.get('/reservaciones-usuarios', validarCookie, reservacionesUsuario)
 apiReservaciones.put('/actualizarReservacion', validarCookie, actualizarReservacion);
