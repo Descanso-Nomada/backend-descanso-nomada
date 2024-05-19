c22fb3a400e46349f40a60c6937d68ce15038401
HaroruDa3 <harol.kock34@gmail.com>
2024-05-18 19:58:23 -0600

    problemas con rutas especificas

--- routes/apiHoteles.js
+++ routes/apiHoteles.js
@@ -1,18 +1,19 @@
 import Express from 'express';
 const apiHoteles = Express();
 import multer from 'multer';
+import { validarCookie } from '../helpers/helpersCookie.js';
 const storage = multer.memoryStorage();
-const upload = multer({storage:storage});
-import { registrarHotel, borrarHotel, mostrarHoteles, mostrarHotel ,hotelesInactivos,cambiarEstadoHotel} from '../controllers/hotelesController.js';
+const upload = multer({ storage: storage });
+import { registrarHotel, borrarHotel, mostrarHoteles, mostrarHotel, hotelesInactivos, cambiarEstadoHotel } from '../controllers/hotelesController.js';
 import { validateRegistroHotel } from '../validators/registroValidator.js';
 
-apiHoteles.get('', mostrarHoteles);
+apiHoteles.get('/inactivos', validarCookie, hotelesInactivos);
 apiHoteles.get('/:id', mostrarHotel);
-apiHoteles.get('/inactivos', hotelesInactivos);
-apiHoteles.post('', upload.single('image'),validateRegistroHotel, registrarHotel);
+apiHoteles.get('', mostrarHoteles);
+apiHoteles.post('', upload.single('image'), validateRegistroHotel, registrarHotel);
 apiHoteles.put('/estado/:id', cambiarEstadoHotel);
 apiHoteles.delete('/:id', borrarHotel);
 
-export { 
+export {
     apiHoteles
 };
